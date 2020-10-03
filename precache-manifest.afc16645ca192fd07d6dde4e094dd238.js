d5abd00e343ff5f8e892b644bd6d08e0b9dad4d4
derek <dereksamuel@gmail.com>
2020-10-03 13:28:07 -0500

    My first commit

---
+++ precache-manifest.afc16645ca192fd07d6dde4e094dd238.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e999c90a1b2ad33d586dab00edf80df8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db8f5995676464a5f4d",
+    "url": "/static/css/main.19502aaa.chunk.css"
+  },
+  {
+    "revision": "afb05b6c533fc1a5ab83",
+    "url": "/static/js/2.024c1146.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.024c1146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db8f5995676464a5f4d",
+    "url": "/static/js/main.55a10a43.chunk.js"
+  },
+  {
+    "revision": "95f3b2b9463e8cf8ecd8",
+    "url": "/static/js/runtime-main.155eb9ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
